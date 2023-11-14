938246d868154aae090260f76c7b7034e2161114
Sheru595 <alexmichaud.18@gmail.com>
2023-11-14 09:13:34 +0100

    first commit

---
+++ index.php
@@ -0,0 +1,15 @@
+#!/usr/bin/env php
+<?php
+// application.php
+
+require __DIR__.'/vendor/autoload.php';
+
+use Symfony\Component\Console\Application;
+
+$application = new Application();
+
+// ... register commands
+
+$application->run();
+
+// Font : Slant Relief, Bloody, Electronic, THIS, Broadway, Ivrit, NScript, Abraxis(-Big), Cheese, PsY
